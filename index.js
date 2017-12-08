5dde6732f40c8a0bc0995183c98d0cfe93684007
jong86 <jon_gaspar@hotmail.com>
2017-12-07 22:39:16 -0800

    Moved app files into /src/ folder

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import appReducers from './reducers'
-import App from './App.js'
+import appReducers from './src/reducers'
+import App from './src/App.js'
 
 let store = createStore(appReducers)
 
