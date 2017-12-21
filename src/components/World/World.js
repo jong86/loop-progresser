9b040e746b21dfc32a631f0f24f9e265049b6c4b
jong86 <jon_gaspar@hotmail.com>
2017-12-20 23:25:42 -0800

    App now starts zoomed in on centered multiTrack (preliminary behavior)

--- src/components/World/World.js
+++ src/components/World/World.js
@@ -5,23 +5,37 @@ import { Ionicons } from '@expo/vector-icons';
 import MultiTrackContainer from '../../containers/MultiTrack/MultiTrackContainer';
 import styles from './_styles_World';
 
+const multiplierMain = 1.2
+const multiplierMap = 8
 
 export default class World extends Component {
+
+  componentDidMount() {
+    console.log('this.refs.sv', this.refs.sv);
+
+    this.refs.sv.scrollTo({
+      // Need to change this to actually get the x and y of the specific multiTrack:
+      x: Dimensions.get('screen').width * multiplierMap * multiplierMain / 2 - Dimensions.get('screen').width / 2,
+      y: Dimensions.get('screen').height * multiplierMap * multiplierMain / 2 - Dimensions.get('screen').height / 2,
+      animated: false
+    })
+  }
+
   render() {
     console.log(Dimensions.get('screen'))
-
     return (
       <ScrollView
         contentContainerStyle={styles.main}
         maximumZoomScale={1.0}
         minimumZoomScale={0.1}
         horizontal
+        ref='sv'
       >
         <View
           style={styles.map}
         >
 
-          <MultiTrackContainer  />
+          <MultiTrackContainer />
 
         </View>
       </ScrollView>
