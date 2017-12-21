b7d2f8ee42d1f84cc508d3e972ec7ce034ef44ad
jong86 <jon_gaspar@hotmail.com>
2017-12-21 08:46:03 -0800

    Changed ScrollView ref to 'scrollView'

--- src/components/World/World.js
+++ src/components/World/World.js
@@ -13,7 +13,7 @@ export default class World extends Component {
   componentDidMount() {
     console.log('this.refs.sv', this.refs.sv);
 
-    this.refs.sv.scrollTo({
+    this.refs.scrollView.scrollTo({
       // Need to change this to actually get the x and y of the specific multiTrack:
       x: Dimensions.get('screen').width * multiplierMap * multiplierMain / 2 - Dimensions.get('screen').width / 2,
       y: Dimensions.get('screen').height * multiplierMap * multiplierMain / 2 - Dimensions.get('screen').height / 2,
@@ -29,7 +29,7 @@ export default class World extends Component {
         maximumZoomScale={1.0}
         minimumZoomScale={0.1}
         horizontal
-        ref='sv'
+        ref='scrollView'
       >
         <View
           style={styles.map}
