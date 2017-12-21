e17b3b385f1ae0d89777e9e65cefc03673146613
jong86 <jon_gaspar@hotmail.com>
2017-12-20 21:08:50 -0800

    Changed to Expo SDK v24.0.0

---
+++ src/components/World/World.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import styles from './_styles_World';
+
+
+export default class World extends Component {
+  render() {
+    return (
+      <View
+        style={styles.main}
+      >
+      </View>
+    )
+  }
+}
---
+++ src/components/World/_styles_World.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  main: {
+    backgroundColor: '#226',
+    width: 80,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
